baa66f86c0324c0bb7d607820b0f3f3b3ef15569
nicschumann <nic@nicschumann.co>
2024-07-23 22:43:57 -0400

    [feat] adds test to check if parallel execution across the soup still works

--- bff/test/test_interp.py
+++ bff/test/test_interp.py
@@ -12,120 +12,178 @@ class State:
     running: list[bool]
 
 
-tests = [
-    (
-        State(
-            soup=[[[0, 0, 1], [0, 0, 0], [0, 0, 0]]], data=[[0, 0, 0]], running=[True]
+def setup_tests():
+    return [
+        (
+            State(
+                soup=[[[0, 0, 1], [0, 0, 0], [0, 0, 0]]],
+                data=[[0, 0, 0]],
+                running=[True],
+            ),
+            State(
+                soup=[[[0, 0, 1], [0, 0, 0], [0, 0, 0]]],
+                data=[[1, 2, 0]],
+                running=[True],
+            ),
         ),
-        State(
-            soup=[[[0, 0, 1], [0, 0, 0], [0, 0, 0]]], data=[[1, 2, 0]], running=[True]
+        (
+            State(
+                soup=[[[0, 0, 2], [0, 0, 0], [0, 0, 0]]],
+                data=[[0, 0, 0]],
+                running=[True],
+            ),
+            State(
+                soup=[[[0, 0, 2], [0, 0, 0], [0, 0, 0]]],
+                data=[[1, 1, 0]],
+                running=[True],
+            ),
         ),
-    ),
-    (
-        State(
-            soup=[[[0, 0, 2], [0, 0, 0], [0, 0, 0]]], data=[[0, 0, 0]], running=[True]
+        (
+            State(
+                soup=[[[0, 0, 3], [0, 0, 0], [0, 0, 0]]],
+                data=[[0, 0, 0]],
+                running=[True],
+            ),
+            State(
+                soup=[[[0, 0, 3], [0, 0, 0], [0, 0, 0]]],
+                data=[[1, 0, 2]],
+                running=[True],
+            ),
         ),
-        State(
-            soup=[[[0, 0, 2], [0, 0, 0], [0, 0, 0]]], data=[[1, 1, 0]], running=[True]
+        (
+            State(
+                soup=[[[0, 0, 4], [0, 0, 0], [0, 0, 0]]],
+                data=[[0, 0, 0]],
+                running=[True],
+            ),
+            State(
+                soup=[[[0, 0, 4], [0, 0, 0], [0, 0, 0]]],
+                data=[[1, 0, 1]],
+                running=[True],
+            ),
         ),
-    ),
-    (
-        State(
-            soup=[[[0, 0, 3], [0, 0, 0], [0, 0, 0]]], data=[[0, 0, 0]], running=[True]
+        (
+            State(
+                soup=[[[0, 0, 5], [0, 0, 0], [0, 0, 0]]],
+                data=[[0, 1, 0]],
+                running=[True],
+            ),
+            State(
+                soup=[[[0, 0, 5], [0, 0, 255], [0, 0, 0]]],
+                data=[[1, 1, 0]],
+                running=[True],
+            ),
         ),
-        State(
-            soup=[[[0, 0, 3], [0, 0, 0], [0, 0, 0]]], data=[[1, 0, 2]], running=[True]
+        (
+            State(
+                soup=[[[0, 0, 6], [0, 0, 0], [0, 0, 0]]],
+                data=[[0, 1, 0]],
+                running=[True],
+            ),
+            State(
+                soup=[[[0, 0, 6], [0, 0, 1], [0, 0, 0]]],
+                data=[[1, 1, 0]],
+                running=[True],
+            ),
         ),
-    ),
-    (
-        State(
-            soup=[[[0, 0, 4], [0, 0, 0], [0, 0, 0]]], data=[[0, 0, 0]], running=[True]
+        (
+            State(
+                soup=[[[0, 0, 7], [0, 0, 4], [0, 0, 5]]],
+                data=[[0, 1, 2]],
+                running=[True],
+            ),
+            State(
+                soup=[[[0, 0, 7], [0, 0, 4], [0, 0, 4]]],
+                data=[[1, 1, 2]],
+                running=[True],
+            ),
         ),
-        State(
-            soup=[[[0, 0, 4], [0, 0, 0], [0, 0, 0]]], data=[[1, 0, 1]], running=[True]
+        (
+            State(
+                soup=[[[0, 0, 8], [0, 0, 4], [0, 0, 5]]],
+                data=[[0, 1, 2]],
+                running=[True],
+            ),
+            State(
+                soup=[[[0, 0, 8], [0, 0, 5], [0, 0, 5]]],
+                data=[[1, 1, 2]],
+                running=[True],
+            ),
         ),
-    ),
-    (
-        State(
-            soup=[[[0, 0, 5], [0, 0, 0], [0, 0, 0]]], data=[[0, 1, 0]], running=[True]
+        (
+            State(
+                soup=[[[0, 0, 9], [0, 0, 0], [0, 0, 10]]],
+                data=[[0, 1, 0]],
+                running=[True],
+            ),
+            State(
+                soup=[[[0, 0, 9], [0, 0, 0], [0, 0, 10]]],
+                data=[[2, 1, 0]],
+                running=[True],
+            ),
         ),
-        State(
-            soup=[[[0, 0, 5], [0, 0, 255], [0, 0, 0]]], data=[[1, 1, 0]], running=[True]
+        (
+            State(
+                soup=[[[0, 0, 9], [0, 0, 0], [0, 0, 0]]],
+                data=[[0, 1, 0]],
+                running=[True],
+            ),
+            State(
+                soup=[[[0, 0, 9], [0, 0, 0], [0, 0, 0]]],
+                data=[[0, 1, 0]],
+                running=[False],
+            ),
         ),
-    ),
-    (
-        State(
-            soup=[[[0, 0, 6], [0, 0, 0], [0, 0, 0]]], data=[[0, 1, 0]], running=[True]
+        (
+            State(
+                soup=[[[0, 0, 9], [0, 0, 1], [0, 0, 0]]],
+                data=[[0, 1, 0]],
+                running=[True],
+            ),
+            State(
+                soup=[[[0, 0, 9], [0, 0, 1], [0, 0, 0]]],
+                data=[[1, 1, 0]],
+                running=[True],
+            ),
         ),
-        State(
-            soup=[[[0, 0, 6], [0, 0, 1], [0, 0, 0]]], data=[[1, 1, 0]], running=[True]
+        (
+            State(
+                soup=[[[0, 0, 9], [0, 0, 1], [0, 0, 10]]],
+                data=[[2, 1, 0]],
+                running=[True],
+            ),
+            State(
+                soup=[[[0, 0, 9], [0, 0, 1], [0, 0, 10]]],
+                data=[[0, 1, 0]],
+                running=[True],
+            ),
         ),
-    ),
-    (
-        State(
-            soup=[[[0, 0, 7], [0, 0, 4], [0, 0, 5]]], data=[[0, 1, 2]], running=[True]
+        (
+            State(
+                soup=[[[0, 0, 0], [0, 0, 1], [0, 0, 10]]],
+                data=[[2, 1, 0]],
+                running=[True],
+            ),
+            State(
+                soup=[[[0, 0, 0], [0, 0, 1], [0, 0, 10]]],
+                data=[[2, 1, 0]],
+                running=[False],
+            ),
         ),
-        State(
-            soup=[[[0, 0, 7], [0, 0, 4], [0, 0, 4]]], data=[[1, 1, 2]], running=[True]
+        (
+            State(
+                soup=[[[0, 0, 9], [0, 0, 10], [0, 0, 0]]],
+                data=[[1, 2, 0]],
+                running=[True],
+            ),
+            State(
+                soup=[[[0, 0, 9], [0, 0, 10], [0, 0, 0]]],
+                data=[[2, 2, 0]],
+                running=[True],
+            ),
         ),
-    ),
-    (
-        State(
-            soup=[[[0, 0, 8], [0, 0, 4], [0, 0, 5]]], data=[[0, 1, 2]], running=[True]
-        ),
-        State(
-            soup=[[[0, 0, 8], [0, 0, 5], [0, 0, 5]]], data=[[1, 1, 2]], running=[True]
-        ),
-    ),
-    (
-        State(
-            soup=[[[0, 0, 9], [0, 0, 0], [0, 0, 10]]], data=[[0, 1, 0]], running=[True]
-        ),
-        State(
-            soup=[[[0, 0, 9], [0, 0, 0], [0, 0, 10]]], data=[[2, 1, 0]], running=[True]
-        ),
-    ),
-    (
-        State(
-            soup=[[[0, 0, 9], [0, 0, 0], [0, 0, 0]]], data=[[0, 1, 0]], running=[True]
-        ),
-        State(
-            soup=[[[0, 0, 9], [0, 0, 0], [0, 0, 0]]], data=[[0, 1, 0]], running=[False]
-        ),
-    ),
-    (
-        State(
-            soup=[[[0, 0, 9], [0, 0, 1], [0, 0, 0]]], data=[[0, 1, 0]], running=[True]
-        ),
-        State(
-            soup=[[[0, 0, 9], [0, 0, 1], [0, 0, 0]]], data=[[1, 1, 0]], running=[True]
-        ),
-    ),
-    (
-        State(
-            soup=[[[0, 0, 9], [0, 0, 1], [0, 0, 10]]], data=[[2, 1, 0]], running=[True]
-        ),
-        State(
-            soup=[[[0, 0, 9], [0, 0, 1], [0, 0, 10]]], data=[[0, 1, 0]], running=[True]
-        ),
-    ),
-    (
-        State(
-            soup=[[[0, 0, 0], [0, 0, 1], [0, 0, 10]]], data=[[2, 1, 0]], running=[True]
-        ),
-        State(
-            soup=[[[0, 0, 0], [0, 0, 1], [0, 0, 10]]], data=[[2, 1, 0]], running=[False]
-        ),
-    ),
-    (
-        State(
-            soup=[[[0, 0, 9], [0, 0, 10], [0, 0, 0]]], data=[[1, 2, 0]], running=[True]
-        ),
-        State(
-            soup=[[[0, 0, 9], [0, 0, 10], [0, 0, 0]]], data=[[2, 2, 0]], running=[True]
-        ),
-    ),
-]
+    ]
+
 
 ids = [
     " < (single_program) ",
@@ -145,7 +203,7 @@ ids = [
 ]
 
 
-@pytest.mark.parametrize(("before", "after"), tests, ids=ids)
+@pytest.mark.parametrize(("before", "after"), setup_tests(), ids=ids)
 def test(before: State, after: State):
     soup_before = torch.tensor(before.soup, dtype=torch.int32)
     data_before = torch.tensor(before.data, dtype=torch.int32)
@@ -156,6 +214,37 @@ def test(before: State, after: State):
     soup_after = torch.tensor(after.soup, dtype=torch.int32)
     data_after = torch.tensor(after.data, dtype=torch.int32)
     running_after = torch.tensor(after.running)
+
+    assert torch.equal(soup_before, soup_after)
+    assert torch.equal(data_before, data_after)
+    assert torch.equal(running_before, running_after)
+
+
+def test_parallel():
+    """This test runs each of the individually run tests above. However,
+    it runs all 14 tests in parallel, to ensure there is no "weird crosstalk"
+    or mistaken indexing between the tests; we want to ensure the parallelism
+    just parallelizes, nothing else."""
+
+    tests = setup_tests()
+
+    soup_before = torch.tensor(
+        list(map(lambda s: s[0].soup[0], tests)), dtype=torch.int32
+    )
+    data_before = torch.tensor(
+        list(map(lambda s: s[0].data[0], tests)), dtype=torch.int32
+    )
+    running_before = torch.tensor(list(map(lambda s: s[0].running[0], tests)))
+
+    step(soup_before, data_before, running_before, instruction_space_size=256)
+
+    soup_after = torch.tensor(
+        list(map(lambda s: s[1].soup[0], tests)), dtype=torch.int32
+    )
+    data_after = torch.tensor(
+        list(map(lambda s: s[1].data[0], tests)), dtype=torch.int32
+    )
+    running_after = torch.tensor(list(map(lambda s: s[1].running[0], tests)))
 
     assert torch.equal(soup_before, soup_after)
     assert torch.equal(data_before, data_after)
